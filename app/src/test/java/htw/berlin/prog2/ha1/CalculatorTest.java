1d8247e52825d360ff19e873375d1ced7352a445
maxherget <hergetmax@icloud.com>
2021-10-14 14:23:36 +0200

    adds 1. subtask: Test for single-digit addition, passes without changing the code under test.

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -42,16 +42,16 @@ class CalculatorTest {
 
     //TODO hier weitere Tests erstellen
     @Test
-    @DisplayName("should display result afer subtract one number from another")
-    void testSubtraction() {
+    @DisplayName("should display result after adding two positive single-digit numbers")
+     void testSubtraction() {
         Calculator calc = new Calculator();
 
-        calc.pressDigitKey(3);
-        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressBinaryOperationKey("+");
         calc.pressDigitKey(1);
         calc.pressEqualsKey();
 
-        String expected = "1";
+        String expected = "2";
         String actual = calc.readScreen();
 
         assertEquals(expected, actual);
