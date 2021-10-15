6555e05ca19ddab1fedac567cd51d516cb6aaad4
Nour Al Hassan <Nour.AlHassan@Student.HTW-Berlin.de>
2021-10-15 15:53:11 +0200

    1. Commit für neuer grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -27,7 +27,7 @@ class CalculatorTest {
     }
 
     @Test
-    @DisplayName("should display result after getting the square root of two")
+    @DisplayName ("should display result after getting the square root of two")
     void testSquareRoot() {
         Calculator calc = new Calculator();
 
@@ -41,5 +41,26 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after subtracting two positive multi-digit numbers")
+    void testPositiveSubtraktion() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressDigitKey(0);
+
+
+        String expected = "10";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+
+
 }
 
