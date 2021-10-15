24b16e10b7bb9d5ef1469432c871692f7700d194
mbHng <buiminh2000@gmx.de>
2021-10-15 17:00:02 +0200

    Änderung: 1. dotKey BugFix, keine mehreren Punkte bein einer Zahl 2. Teilen durch Null --> Screen zeigt jetzt "ERROR"

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator {
      * Beim zweimaligem Drücken, oder wenn bereits ein Trennzeichen angezeigt wird, passiert nichts.
      */
     public void pressDotKey() {
-        if (!screen.endsWith(".")) screen = screen + ".";
+        if (!screen.contains(".")) screen = screen + ".";
 
     }
 
@@ -140,6 +140,6 @@ public class Calculator {
         screen = Double.toString(result);
         if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
         if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-
+        if (screen.equals("Infinity")) screen = "ERROR" ;
     }
 }
