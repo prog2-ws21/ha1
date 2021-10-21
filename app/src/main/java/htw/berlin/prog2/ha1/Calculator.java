7244b3acde7a5794761feb0aa98084ac44e0bbe4
nicomarantos <nicolas-m@live.de>
2021-10-21 19:32:48 +0200

    1. Roter test (NEU) und Fix des Tests

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -125,16 +125,7 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
 
-    public void moreThanOneOperator () {
-        if (screen.contains("/")) {
-
-
-        }
-
-      //  switch (operation) {
-         //   case
-       // }
-    }
+   
     public void pressEqualsKey() {
         var result = switch(latestOperation) {
             case "+" -> latestValue + Double.parseDouble(screen);
@@ -149,7 +140,7 @@ public class Calculator {
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-        if (screen.contains("/") & screen.contains("+"));
-        moreThanOneOperator();
+
     }
+
 }
