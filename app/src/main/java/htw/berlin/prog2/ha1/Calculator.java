9bcfdf847753de3e52d339f492327909137b92c2
Ekaterina Borovskaya <Ekaterina.Borovskaya@Student.HTW-Berlin.de>
2021-10-23 00:27:20 +0300

    Second bug fixed

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -93,7 +93,11 @@ public class Calculator {
      * Beim zweimaligem Drücken, oder wenn bereits ein Trennzeichen angezeigt wird, passiert nichts.
      */
     public void pressDotKey()  {
-            if (!screen.endsWith(".")){screen = screen + ".";}
+        while (!screen.contains(".")){
+            if (!screen.endsWith(".")){
+                screen = screen + ".";
+            }
+        }
     }
 
     /**
