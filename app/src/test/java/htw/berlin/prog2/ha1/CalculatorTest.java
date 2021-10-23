879db9c0581253ad501ad8995d732a9c287b0702
JordanHubb <72967048+JordanHubb@users.noreply.github.com>
2021-10-23 16:50:20 +0200

    Zweiter roter Test. Beim teilen durch Null ist das Ergebnis unendlich

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -74,6 +74,20 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("should display error after dividing with number 0")
+    void testDivisionZero() {
+        Calculator calc = new Calculator();
 
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
