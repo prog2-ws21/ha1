fc0e9a543be54e4a54cc34b2b61eacce3aed8f5c
JordanHubb <72967048+JordanHubb@users.noreply.github.com>
2021-10-23 11:58:46 +0200

    Meine erster Test multipliziert zwei Zahlen miteinander und war direkt grün

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -40,6 +40,20 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
-    //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("should display result after multiply two positiv single digit number")
+    void testMultiply(){
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "4";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
