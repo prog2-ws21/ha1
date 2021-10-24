a03122bbe2724b1c53101ea4d950782793bc5c6c
Marco Hamiti <marcohamiti1@gmail.com>
2021-10-24 16:16:14 +0200

    red test after trying to add a negative number with a positive number

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -31,9 +31,13 @@ public class Calculator {
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
 
+        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        {
         screen = screen + digit;
+        }
+
+
     }
 
     /**
--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -57,6 +57,25 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+
+    @Test
+    @DisplayName("should display result after adding a negative number with a positive number")
+    void calculatorCanDoMinusTwoPlusFive() {
+        Calculator calc = new Calculator();
+        calc.pressNegativeKey();
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(5);
+        calc.pressEqualsKey();
+
+        String expected = "3";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+
+
     //TODO hier weitere Tests erstellen
 }
 
