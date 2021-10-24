de5d3400a114c0150c44efd8139fccfd8ac11a41
Max Gurgel <s0569230@htw-berlin.de>
2021-10-24 14:37:12 +0200

    adding first green test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -8,38 +8,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Retro calculator")
 class CalculatorTest {
 
-    @Test
-    @DisplayName("should display result after adding two positive multi-digit numbers")
-    void testPositiveAddition() {
+	@Test
+	@DisplayName("should display result after adding two positive multi-digit numbers")
+	void testPositiveAddition() {
+		Calculator calc = new Calculator();
+
+		calc.pressDigitKey(2);
+		calc.pressDigitKey(0);
+		calc.pressBinaryOperationKey("+");
+		calc.pressDigitKey(2);
+		calc.pressDigitKey(0);
+		calc.pressEqualsKey();
+
+		String expected = "40";
+		String actual = calc.readScreen();
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	@DisplayName("should display result after getting the square root of two")
+	void testSquareRoot() {
+		Calculator calc = new Calculator();
+
+		calc.pressDigitKey(2);
+		calc.pressUnaryOperationKey("√");
+
+		String expected = "1.41421356";
+		String actual = calc.readScreen();
+
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+    @DisplayName("should display result after subtracting two positive numbers")
+    void testPositiveSubtraction() {
         Calculator calc = new Calculator();
 
-        calc.pressDigitKey(2);
-        calc.pressDigitKey(0);
-        calc.pressBinaryOperationKey("+");
-        calc.pressDigitKey(2);
-        calc.pressDigitKey(0);
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(3);
+
         calc.pressEqualsKey();
 
-        String expected = "40";
+        String expected = "3";
         String actual = calc.readScreen();
 
         assertEquals(expected, actual);
     }
 
-    @Test
-    @DisplayName("should display result after getting the square root of two")
-    void testSquareRoot() {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(2);
-        calc.pressUnaryOperationKey("√");
-
-        String expected = "1.41421356";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
-
-    //TODO hier weitere Tests erstellen
+	// TODO hier weitere Tests erstellen
 }
-
