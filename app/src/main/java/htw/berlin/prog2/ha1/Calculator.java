65ab7ff3f88d5613b1f5fd3f4fe0dcf4b332a3ef
Alicia Sternkopf <alicia.sternkopf@web.de>
2021-10-27 18:36:16 +0200

    bugfix rote Tests

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -123,10 +123,14 @@ public class Calculator {
             case "-" -> latestValue - Double.parseDouble(screen);
             case "x" -> latestValue * Double.parseDouble(screen);
             case "/" -> latestValue / Double.parseDouble(screen);
+            case "%" -> Double.parseDouble(screen);
+            case "1/x" -> Double.parseDouble(screen);
+
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+
     }
 }
