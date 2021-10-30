e7daea3f3633af1755388732ed7a0f6a0dce9457
erens <erenserkantas@gmail.com>
2021-10-30 14:22:40 +0200

    5. Commit: 2. roter Test Fix (Nachgebessert)

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -30,8 +30,8 @@ public class Calculator {
      */
     public void pressDigitKey(int digit) {
         if(digit > 9 || digit < 0) throw new IllegalArgumentException();
-
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        if(screen.equals("-0")) screen = "-";
+        else if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
 
         screen = screen + digit;
     }
