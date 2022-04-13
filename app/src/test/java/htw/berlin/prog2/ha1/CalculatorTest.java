aa94d15b051eb36ea613226bdd961849408ecd03
OhNotWilliam <86485831+OhNotWilliam@users.noreply.github.com>
2022-04-13 10:20:30 +0200

    Remove all new tests (keep fixes)

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -42,62 +42,6 @@ class CalculatorTest {
 
     //TODO hier weitere Tests erstellen
 
-    @Test
-    @DisplayName("Tests whether calculator is cleared. Screen should be 0, latest op empty string and latest value 0.0")
-    void testClear() {
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(2);
-        calc.pressBinaryOperationKey("+");
-        calc.pressClearKey();
-
-        String expected = "0";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-
-        expected = "";
-        actual = calc.getLatestOperation();
-
-        assertEquals(expected, actual);
-
-        double exp = 0.0;
-        double act = calc.getLatestValue();
-
-        assertEquals(exp, act);
-
-    }
-
-    @Test
-    @DisplayName("Test whether divide by zero throws 'ERROR'")
-    public void testDivideZero(){
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(2);
-        calc.pressBinaryOperationKey("/");
-        calc.pressDigitKey(0);
-        calc.pressEqualsKey();
-
-        String expected = "ERROR";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    @DisplayName("Test whether negative root throws 'ERROR'")
-    public void testNegativeRootIsError(){
-        Calculator calc = new Calculator();
-
-        calc.pressDigitKey(2);
-        calc.pressNegativeKey();
-        calc.pressUnaryOperationKey("√");
-
-        String expected = "ERROR";
-        String actual = calc.readScreen();
-
-        assertEquals(expected, actual);
-    }
 
 }
 
